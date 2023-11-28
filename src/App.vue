05760df3e77a84ad9a5300d65ce5a907d0f2040f
Solo Coder <kisliydanecka@gmail.com>
2023-11-29 02:22:18 +0300

    'start'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
